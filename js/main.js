0e219a40ed9094f5034a3b7d79ba3c76688d064d
Reagan Brasch <reaganbrasch@gmail.com>
2018-08-18 21:12:48 +1000

    Functioning notifications box with deletable elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
- 
+// Delete Notification
+var notif = document.querySelectorAll('.notif-element')
+notif.forEach(btn => {
+	btn.addEventListener('click', function() {
+		var li = this.parentNode
+		li.remove()
+	})
+})
