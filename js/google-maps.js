6fecbc9cfc458d2ba76a53ddd6a7b2dd4fc4f44b
Reagan Brasch <reaganbrasch@gmail.com>
2018-08-22 20:31:03 +1000

    designed blog.html and with stylesheets

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,8 +1,11 @@
-function myMap() {
+// Function which initiailises google maps
+function gosfordMap() {
+	// Object variable giving information to google maps
 	var mapOptions = {
 		 center: new google.maps.LatLng(-33.439, 151.347),
 		 zoom: 17,
 		 mapTypeId: google.maps.MapTypeId.HYBRID
 	}
+	// Instantiating map object
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	}
