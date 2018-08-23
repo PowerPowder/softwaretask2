2736aa33048972d177838600be9f88d1efce1311
Reagan Brasch <reaganbrasch@gmail.com>
2018-08-23 21:33:42 +1000

    created stub for chat system, uses json file to generate messages

---
+++ js/chat.js
@@ -0,0 +1,18 @@
+sportsCarnival = {
+	"Person1": "Blah blah blah",
+	"Person2": "Blah blah",
+	"Person3": "blablah"
+}
+
+swimmingCarnival = {
+	"Message1": "Blah blah blah blaaaaaaaahhhhh",
+	"Message2": "blaaaaaaaaaaaaaaaaaaaaaaahhhhh"
+}
+
+let message1 = document.getElementsByClassName("text-message")[0];
+let message2 = document.getElementsByClassName("text-message")[1];
+let message3 = document.getElementsByClassName("text-message")[2];
+
+message1.innerHTML = sportsCarnival.Person1;
+message2.innerHTML = sportsCarnival.Person2;
+message3.innerHTML = sportsCarnival.Person3;
